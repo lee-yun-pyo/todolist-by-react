b926333cb49f50b6f3da6996e4423c66b4de63e2
YTim <yun2193@hanmail.net>
2023-01-19 21:18:55 +0900

    #10 버튼 카테고리 우상단 개수 출력

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,6 +6,7 @@ import {
   toDoSelector,
   AddListModalState,
   categoriesState,
+  toDoState,
 } from "../atoms";
 import styled from "styled-components";
 import ModalAddList from "../etc/ModalAddList";
@@ -31,35 +32,55 @@ const Title = styled.h1`
   font-weight: 600;
 `;
 
-const BtnDiv = styled.div`
+const BtnContainer = styled.div`
   display: grid;
-  gap: 10px;
+  gap: 15px;
   grid-template-columns: repeat(4, 1fr);
-  margin-top: 30px;
+  margin-top: 50px;
 `;
 
-const Button = styled.button<{ isActive: boolean }>`
-  padding: 15px 10px;
-  font-size: 20px;
-  background-color: ${(props) => (props.isActive ? "#55efc4" : "#00b894")};
-  border: none;
-  cursor: pointer;
+const BtnDiv = styled.div<{ isActive: boolean }>`
+  position: relative;
   border-radius: 10px;
-  color: white;
   box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.3);
+  max-width: 110px;
+  background-color: ${(props) => (props.isActive ? "#40739e" : "#273c75")};
+  &:hover {
+    background-color: #40739e;
+  }
+`;
+const Button = styled.button`
+  cursor: pointer;
+  color: white;
+  border: none;
   transition: all 0.1s ease-in-out;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  max-width: 110px;
-  &:hover {
-    background-color: #55efc4;
-  }
+  background-color: transparent;
+  padding: 15px 10px;
+  font-size: 20px;
+  width: 100%;
+`;
+
+const Circle = styled.span<{ count: number }>`
+  display: ${(props) => (props.count > 0 ? "block" : "none")};
+  background-color: #e84118;
+  font-size: 19px;
+  color: #fff;
+  padding: 6px 10px;
+  border-bottom-left-radius: 50%;
+  border-bottom-right-radius: 50%;
+  border-top-left-radius: 50%;
+  border-top-right-radius: 50%;
+  position: absolute;
+  top: -10px;
+  right: -12px;
 `;
 
 const Line = styled.div`
   height: 1px;
-  width: 90%;
+  width: 100%;
   background-color: #b2bec3;
   margin: 30px 0;
 `;
@@ -74,6 +95,7 @@ const TodoLists = styled.ul`
 
 function TodoList() {
   const todos = useRecoilValue(toDoSelector);
+  const allTodos = useRecoilValue(toDoState);
   const [category, setCategory] = useRecoilState(toDoCategory);
   const setDisplayModal = useSetRecoilState(AddListModalState);
   const categories = useRecoilValue(categoriesState);
@@ -83,20 +105,30 @@ function TodoList() {
   const showModal = () => {
     setDisplayModal(true);
   };
+  const categoryInTodos = allTodos.map((todo) => todo.category);
   return (
     <Container>
       <TitleAndButton>
         <Title>TO DO LIST</Title>
-        <BtnDiv>
+        <BtnContainer>
           {categories.map((cate) => (
-            <Button key={cate} onClick={onClick} isActive={category === cate}>
-              {cate}
-            </Button>
+            <BtnDiv key={cate} isActive={category === cate}>
+              <Button key={cate} onClick={onClick}>
+                {cate}
+              </Button>
+              <Circle
+                count={categoryInTodos.filter((todo) => todo === cate).length}
+              >
+                {categoryInTodos.filter((todo) => todo === cate).length}
+              </Circle>
+            </BtnDiv>
           ))}
-          <Button isActive={false} onClick={showModal}>
-            <i className="fa-solid fa-plus fa-lg"></i>
-          </Button>
-        </BtnDiv>
+          <BtnDiv isActive={false}>
+            <Button onClick={showModal}>
+              <i className="fa-solid fa-plus fa-lg"></i>
+            </Button>
+          </BtnDiv>
+        </BtnContainer>
       </TitleAndButton>
       <Line />
       <SubContainer>
